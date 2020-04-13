b91862996111676826cca6d305a35bb38457ee82
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-04-13 11:49:12 +0200

    Getting started

---
+++ src/main/java/com/registerbook/registerbook/RegisterbookApplication.java
@@ -0,0 +1,13 @@
+package com.registerbook.registerbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registerbook/registerbook/RegisterbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registerbook.registerbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
