78c9236a8163f1201378c1adf2d777cc2b36077b
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-04-13 20:15:36 +0200

    GetAllMembers working with the front end

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -2,26 +2,73 @@
 app.controller('listmembercontroller',function($scope,$http,$location,$route){
     $http({
         method : 'GET',
-        url : 'http://localhost:8080/register/member'
+        url : 'http://localhost:8080/register/member/'
     }).then(function(response){
         $scope.members = response.data;
-    })
-
-    //edit
-    //delete
+    });
+    $scope.editMember = function(memberId){
+        $location.path("/update-member/" + memberId);
+    }
+    $scope.deleteMember = function(memberId){
+        $http({
+            method : 'DELETE',
+            url : 'http://localhost:8080/register/member/' + memberId
+        }).then(function(response){
+            $location.path("/list-all-members");
+            $route.reload();
+        });
+    }
 });
 
 app.controller('registermembercontroller', function($scope,$http,$location,$route){
     $scope.submitMemberForm = function(){
-        method : 'POST',
-        url : 'http://localhost:8080/'
+        $http({
+                method : 'POST',
+                url : 'http://localhost:8080/register/member',
+                data : $scope.member,
+        }).then(function(response){
+            $location.path("/list-all-members");
+            $route.reload();
+        },function(errorResponse){
+            $scope.errorMessage = errResponse.data.errorMessage;
+        });
     };
+    $scope.resetForm = function(){
+        $scope.member = null;
+    }
 });
 
 app.controller('memberdetailscontroller', function($scope,$http,$location,$route){
-
+    $scope.memberId = $routeParams.id;
+    $http({
+        method : 'GET',
+        url : 'http://localhost:8080/register/member' + $scope.memberId
+    }).then(function(response){
+        $scope.member = response.data;
+    });
+    $scope.submitMemberForm = function(){
+        $http({
+            method : 'POST',
+            url : 'http://localhost:8080/register/member',
+            data : $scope.member,
+        }).then(function(response){
+            $location.path("/list-all-members");
+            $route.reload();
+        },function(errResponse){
+            $scope.errorMessage = errResponse.data.errorMessage;
+        });
+    };
 });
 
 app.controller('searchmembercontroller', function($scope,$http,$location,$route){
-
+    $scope.searchByName = function(){
+        let a = $scope.name;
+        console.log(a);
+        $http({
+            method : 'GET',
+            url : 'http://localhost:8080/register/member/' + a
+        }).then(function(response){
+            $scope.searchedMembers = response.data;
+        });
+    }
 });
