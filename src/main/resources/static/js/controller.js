b7117569a25fe85307fec59e327000445608f47c
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-05-03 17:44:01 +0200

    Js controller refactored

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,10 +1,9 @@
-
 app.controller('listmembercontroller',function($scope,$http,$location,$route){
     $http({
-        method : 'GET',
-        url : 'http://localhost:8080/register/member/'
+            method : 'GET',
+            url : 'http://localhost:8080/register/member/'
     }).then(function(response){
-        $scope.members = response.data;
+            $scope.members = response.data;
     });
 
     $scope.editMember = function(memberId){
@@ -12,11 +11,11 @@ app.controller('listmembercontroller',function($scope,$http,$location,$route){
     }
     $scope.deleteMember = function(memberId){
         $http({
-            method : 'DELETE',
-            url : 'http://localhost:8080/register/member/' + memberId
+                method : 'DELETE',
+                url : 'http://localhost:8080/register/member/' + memberId
         }).then(function(response){
-            $location.path("/list-all-members");
-            $route.reload();
+                $location.path("/list-all-members");
+                $route.reload();
         });
     }
 });
@@ -28,10 +27,10 @@ app.controller('registermembercontroller', function($scope,$http,$location,$rout
                 url : 'http://localhost:8080/register/member/',
                 data : $scope.member
         }).then(function(response){
-            $location.path("/list-all-members");
-            $route.reload();
+                $location.path("/list-all-members");
+                $route.reload();
         },function(errorResponse){
-            $scope.errorMessage = errResponse.data.errorMessage;
+                $scope.errorMessage = errResponse.data.errorMessage;
         });
     };
     $scope.resetForm = function(){
@@ -42,21 +41,22 @@ app.controller('registermembercontroller', function($scope,$http,$location,$rout
 app.controller('memberdetailscontroller', function($scope,$http,$routeParams,$location,$route){
     $scope.memberId = $routeParams.id;
     $http({
-        method : 'GET',
-        url : 'http://localhost:8080/register/member/' + $scope.memberId
+            method : 'GET',
+            url : 'http://localhost:8080/register/member/' + $scope.memberId
     }).then(function(response){
-        $scope.member = response.data;
+            $scope.member = response.data;
     });
+
     $scope.submitMemberForm = function(){
         $http({
-            method : 'POST',
-            url : 'http://localhost:8080/register/member/',
-            data : $scope.member,
+                method : 'POST',
+                url : 'http://localhost:8080/register/member/',
+                data : $scope.member,
         }).then(function(response){
-            $location.path("/list-all-members");
-            $route.reload();
+                $location.path("/list-all-members");
+                $route.reload();
         },function(errResponse){
-            $scope.errorMessage = errResponse.data.errorMessage;
+                $scope.errorMessage = errResponse.data.errorMessage;
         });
     };
 });
@@ -64,12 +64,11 @@ app.controller('memberdetailscontroller', function($scope,$http,$routeParams,$lo
 app.controller('searchmembercontroller', function($scope,$http,$location,$route){
     $scope.searchbyname = function(){
         let a = $scope.name;
-        console.log(a);
         $http({
-            method : 'GET',
-            url : 'http://localhost:8080/register/member/searchname/' + a
+                method : 'GET',
+                url : 'http://localhost:8080/register/member/searchname/' + a
         }).then(function(response){
-            $scope.searchedMembers = response.data;
+                $scope.searchedMembers = response.data;
         });
     }
     $scope.editMember = function(memberId){
@@ -77,11 +76,11 @@ app.controller('searchmembercontroller', function($scope,$http,$location,$route)
     }
         $scope.deleteMember = function(memberId){
             $http({
-                method : 'DELETE',
-                url : 'http://localhost:8080/register/member/' + memberId
+                    method : 'DELETE',
+                    url : 'http://localhost:8080/register/member/' + memberId
             }).then(function(response){
-                $location.path("/list-all-members");
-                $route.reload();
+                    $location.path("/list-all-members");
+                    $route.reload();
             });
         }
         $scope.resetForm = function(){
@@ -95,28 +94,27 @@ app.controller('searchdropdown',function($scope,$http,$location,$route){
     $scope.push = function(){
 
         $scope.property = [$scope.selectedName,$scope.content];
-        console.log($scope.property[0] + " " + $scope.property[1]);
 
         if($scope.content != null && $scope.selectedName != null){
              $http({
-                         method : 'POST',
-                         url : 'http://localhost:8080/register/member/searchproperty',
-                         data : $scope.property,
-                     }).then(function(response){
+                    method : 'POST',
+                    url : 'http://localhost:8080/register/member/searchproperty',
+                    data : $scope.property,
+                    }).then(function(response){
                          $scope.searched = response.data;
-                     },function(errResponse){
+                    },function(errResponse){
                          $scope.errorMessage = errResponse.data.errorMessage;
-                     });
-             }
+                    });
+        }
     };
 });
 app.controller('statistics',function($scope,$http,$location,$route){
     $http({
             method : 'GET',
             url : 'http://localhost:8080/register/member/statistics'
-        }).then(function(response){
+          }).then(function(response){
             $scope.statistics = response.data;
-        });
+          });
 });
 app.controller('newmemberregistration',function($scope,$http,$location,$route){
     $scope.instruments = ['Guitar','Bass Guitar','Drum','Singer','Violin','Harmonica','Rhythm guitar','Keyboard instruments'];
@@ -128,12 +126,12 @@ app.controller('newmemberregistration',function($scope,$http,$location,$route){
                     url : 'http://localhost:8080/register/member/',
                     data : $scope.member
             }).then(function(response){
-                $location.path("/list-all-members");
-                $route.reload();
+                    $location.path("/list-all-members");
+                    $route.reload();
             },function(errorResponse){
-                $scope.errorMessage = errResponse.data.errorMessage;
+                    $scope.errorMessage = errResponse.data.errorMessage;
             });
-        };
+    };
         $scope.resetForm = function(){
             $scope.member = null;
         }
