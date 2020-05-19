22eaf57c5dd0d3e4010de4c6b8a15a7680aa8b1c
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-05-19 16:32:06 +0200

    Working on controllertest

--- src/test/java/com/registerbook/registerbook/controllerTest/MemberControllerTest.java
+++ src/test/java/com/registerbook/registerbook/controllerTest/MemberControllerTest.java
@@ -6,8 +6,6 @@ import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -25,9 +23,33 @@ public class MemberControllerTest extends AbstractTest{
                 .get(url)
                 .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
         int status = mvcResult.getResponse().getStatus();
+
         assertEquals(200,status);
         String content = mvcResult.getResponse().getContentAsString();
         Member[] memberList = super.mapFromJson(content,Member[].class);
         assertTrue(memberList.length>0);
+    }
+    @Test
+    public void createNewMemberTest() throws Exception{
+        String url = "/register/member/";
+        Member testMember = new Member();
+        testMember.setId(new Long(1));
+        testMember.setName("Adam");
+        testMember.setBand("Amon Amarth");
+        testMember.setAddress("Bszh");
+        testMember.setEmail("ld@ld.hu");
+        testMember.setYearOfBirth(1989);
+        testMember.setCountry("Hungary");
+        testMember.setInstrument("Guitar");
+
+        String inputJson = super.mapToJson(testMember);
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
+                .post(url).contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(inputJson))
+                .andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(409,status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content,"Product is created successfully");
     }
 }
