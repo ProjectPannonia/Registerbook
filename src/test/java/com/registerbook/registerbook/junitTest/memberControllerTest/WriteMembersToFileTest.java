82082b3bdb9b08eb235e6efd9bff468a3488ade0
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-06-14 16:19:28 +0200

    writeMembersToFile test
    when file successfully created

--- src/test/java/com/registerbook/registerbook/junitTest/memberControllerTest/WriteMembersToFileTest.java
+++ src/test/java/com/registerbook/registerbook/junitTest/memberControllerTest/WriteMembersToFileTest.java
@@ -8,7 +8,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class WriteMembersToFileTest {
@@ -22,11 +28,32 @@ public class WriteMembersToFileTest {
 
     }
     @Test
-    public void test1(){
+    public void test_writeMembersToFile_When_FileCreated(){
+        String testFileName = "xyz";
+        String expectedResponseText = "File created. Name: " + testFileName + ".txt";
+        HttpStatus expectedStatus = HttpStatus.OK;
 
+        when(memberServiceImplementation.writeMembersToFile(testFileName)).thenReturn(new ResponseEntity<>(expectedResponseText,expectedStatus));
+
+        ResponseEntity nonEmptyResponse = memberController.writeMembersToFile(testFileName);
+        Object responseText = nonEmptyResponse.getBody();
+        HttpStatus responseStatus = nonEmptyResponse.getStatusCode();
+
+        assertTrue(nonEmptyResponse != null);
+        assertTrue(responseStatus != null);
+        assertTrue(responseText != null);
+        assertTrue(responseText instanceof String);
+        assertTrue(responseStatus instanceof HttpStatus);
+        assertTrue(nonEmptyResponse instanceof ResponseEntity);
+        assertEquals(responseText,expectedResponseText);
+        assertEquals(responseStatus,expectedStatus);
+
+        verify(memberServiceImplementation,times(1)).writeMembersToFile(testFileName);
     }
     @After
     public void setToNull(){
+        memberController = null;
+        memberServiceImplementation = null;
 
     }
 }
