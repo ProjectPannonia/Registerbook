c9b27f57bcd9e1f41f603326737d761ef42df78c
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-06-19 16:41:47 +0200

    Update ResourceNotFoundException.java

--- src/main/java/com/registerbook/registerbook/controller/errorHandler/customException/ResourceNotFoundException.java
+++ src/main/java/com/registerbook/registerbook/controller/errorHandler/customException/ResourceNotFoundException.java
@@ -2,11 +2,8 @@ package com.registerbook.registerbook.controller.errorHandler.customException;
 
 
 public class ResourceNotFoundException extends RuntimeException {
-    /*public HttpStatus throwException(){
-        return HttpStatus.NOT_FOUND;
-    }*/
+
     public ResourceNotFoundException(String message){
         super(message);
-        //System.out.println("Not found");
     }
 }
