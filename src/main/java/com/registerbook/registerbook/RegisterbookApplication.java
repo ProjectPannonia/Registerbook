f72742569efebecfe39228996b79e4e463d29de6
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2020-07-01 13:00:26 +0200

    EnableJpaRepo and EntityScan annotation added

--- src/main/java/com/registerbook/registerbook/RegisterbookApplication.java
+++ src/main/java/com/registerbook/registerbook/RegisterbookApplication.java
@@ -2,7 +2,11 @@ package com.registerbook.registerbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.registerbook.registerbook.repository")
+@EntityScan("com.registerbook.registerbook.model.entities")
 @SpringBootApplication
 public class RegisterbookApplication {
 
